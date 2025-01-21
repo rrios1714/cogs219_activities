e491ee45aead64baf72f6e84c1732ea932ca4018
rrios1714 <rrbyam4711@gmail.com>
2025-01-21 15:06:21 -0800

    in class edits, review

--- python/intro_to_psychopy/simple_rt.py
+++ python/intro_to_psychopy/simple_rt.py
@@ -4,12 +4,16 @@ from psychopy import visual, event, core # import the bits of PsychoPy we'll nee
 win = visual.Window([600,600],color="grey", units='pix', checkTiming=False) 
 
 #create a blue circle
-blue_circle = visual.Circle(win,lineColor="grey",fillColor="blue",size=[100,100])
+#blue_circle = visual.Circle(win,lineColor="grey",fillColor="blue",size=[100,100])
+org_circle = visual.Circle(win, lineColor = "white",fillColor = "orange", size=[100,100])
+
 
 #show the blue circle
 #first, draw the blue circle to the back buffer of the window
 #this means that the blue circle won't be displayed right away
-blue_circle.draw()
+#blue_circle.draw()
+org_circle.draw()
+
 #then "flip" the window to show what you just drew
 win.flip()
 
